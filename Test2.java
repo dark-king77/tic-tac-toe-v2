44578bcf755d7298a54aa951b2d76f37cfe6a5f4
dark-king77 <hariharan221121@gmail.com>
2023-12-14 00:05:28 +0530

    adding the gameover class

--- Test2.java
+++ Test2.java
@@ -283,11 +283,69 @@ public static boolean checksum(int a[][]){
 }
 }
 
-
+//the main class that invoks the game arena.
 class tic_tac_toe
 {
 public static void main(String[]args)
 {
  new Simple();
 }
+}
+
+//the gameover class that is invoked when game is over
+
+class gameover extends JFrame
+{
+  
+  JButton b=new JButton();
+  
+  JLabel l1=new JLabel("PLAYER A WON!");
+  
+  JLabel l2=new JLabel("PLAYER B WON!");
+  
+  private ImageIcon img;
+  int x;
+  gameover(int x)
+  {
+    this.x=x;
+    if(x==1)
+    {
+      l1.setBounds(150,300,400,50);
+      Font lf=new Font("verdana",Font.PLAIN,25);
+      l1.setFont(lf);
+      b.setBounds(200,100,100,100);
+      b.setIcon(new ImageIcon("ticx1.png"));
+      add(b);
+      add(l1);
+    }
+    else if(x==2)
+    {
+      l2.setBounds(150,300,400,50);
+      Font lf=new Font("verdana",Font.PLAIN,25);
+      l2.setFont(lf);
+      b.setBounds(200,100,100,100);
+      b.setIcon(new ImageIcon("tico1.png"));
+      add(b);
+      add(l2);
+    }
+    else
+    {
+      JLabel l=new JLabel("MATCH DRAW");
+      JLabel l1=new JLabel("AND");
+      JLabel l2=new JLabel("RESTART THE GAME");
+      l.setBounds(100,150,450,50);
+      l1.setBounds(150,200,450,50);
+      l2.setBounds(40,250,450,50);
+      Font lf=new Font("verdana",Font.BOLD,25);
+      l.setFont(lf);
+      l1.setFont(lf);
+      l2.setFont(lf);
+      add(l);
+      add(l1);
+      add(l2);
+    }
+    setSize(500,500);
+    setLayout(null);
+    setVisible(true);
+  }
 }
